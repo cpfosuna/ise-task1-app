7a5076ce452080033d8c333c643647a00e5290d4
cpfosuna <cparra@fundacionsafa.es>
2025-07-04 23:07:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { ActivitiesMenu } from "./pages/ActivitiesMenu";
+import { MultipleChoice } from "./pages/MultipleChoice";
+import { Matching } from "./pages/Matching";
+import { DragDrop } from "./pages/DragDrop";
+import { ImageSelect } from "./pages/ImageSelect";
+import { ShortAnswer } from "./pages/ShortAnswer";
+import { Results } from "./pages/Results";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<ActivitiesMenu />} />
+        <Route path="/activity/multiple-choice" element={<MultipleChoice />} />
+        <Route path="/activity/matching" element={<Matching />} />
+        <Route path="/activity/drag-drop" element={<DragDrop />} />
+        <Route path="/activity/image-select" element={<ImageSelect />} />
+        <Route path="/activity/short-answer" element={<ShortAnswer />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </Router>
+  );
+}
 
